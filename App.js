2895fae4712b3f620b29b086acf44591a720e5a3
Eliton Camargo <camargoliveira@gmail.com>
2023-04-17 21:12:23 -0300

    Criando navegação com Bottom-tabs

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Adidas from './components/Adidas';
+import { NavigationContainer} from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign } from '@expo/vector-icons';
+
+import Nike from "./components/Nike";
+import Mizuno from "./components/Mizuno";
+import Adidas from "./components/Adidas";
+
+
+
+const Menu = createBottomTabNavigator().Navigator;
+const ItensMenu = createBottomTabNavigator().Screen;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Adidas/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      
+        <Menu
+          screenOptions={{
+            tabBarStyle: {
+              backgroundColor: "#ccc"              
+            },
+            tabBarLabelStyle: {
+              fontSize: 18,
+              fontWeight: 'bold',
+            },
+            tabBarActiveBackgroundColor: "#fff",
+            tabBarInactiveTintColor: "#555",
+            tabBarActiveTintColor: "#222",
+            tabBarLabelPosition: 'beside-icon',
+          }}
+        >
+          <ItensMenu name="Nike" component={Nike} options={{
+            tabBarLabel: 'Nike',
+            tabBarIcon:({color, size})=>(<AntDesign name="check" size={size} color={color} />),
+            tabBarBadge: 3,
+          }}/>
+          <ItensMenu name="Mizuno" component={Mizuno}/>
+          <ItensMenu name="Adidas" component={Adidas}/>
+        </Menu>
+      
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
